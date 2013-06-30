ecaa00ac6484d864c9e492bc7177af18cc3f37a3
Ali Nahm <alisnahm@gmail.com>
2013-06-29 17:19:21 -0700

    new div

--- widgetTypes/stickyNotes.js
+++ widgetTypes/stickyNotes.js
@@ -6,7 +6,7 @@ _.extend(StickyNotes.prototype, {
   render: function() {
     this.html = '<div id="widget_' + this.widgetId + '" style="'+ this.data.style + '" class="widget">';
     this.html += '<h2>Sticky Note</h2>';
-    this.html += this.data.content;
+    this.html += '<div class="stickyData">' + this.data.content + "</div>";
     this.html += '</div>';
   }
 });
