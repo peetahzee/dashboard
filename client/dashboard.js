2229ade9f1a54513bc8c172b7dcd6fbb69168909
Ali Nahm <alisnahm@gmail.com>
2013-06-29 19:39:02 -0700

    merge

--- client/dashboard.js
+++ client/dashboard.js
@@ -41,9 +41,30 @@ Template.dashboard.events({
 Template.widget.events({
     'click': function (event) {
         var idName = "#widget_" + this.widgetId;
-        $(idName).find("textarea").css("display", "block");
-        $(idName).find(".stickyData").css("display", "none");
+        // Want to toggle based on when clicking
+        if ($(idName).find(".stickyData").css("display") === "block") {
+            $(idName).find(".stickyData").css("display", "none");
+            $(idName).find("textarea").css("display", "block");
+            height = $(idName).innerHeight() - $(idName).find("h2").height() - 65;
+            $(idName).find("textarea").css("height", height);
 
+            // Make div not resizable
+            $(idName).resizable( "destroy" )
+        } else {
+            $(idName).find(".stickyData").css("display", "block");
+            $(idName).find("textarea").css("display", "none");
+
+            $(idName).resizable({
+                stop: function(event, ui) {
+                          widgetId = $(this).attr('id').substring(7);
+                          toSet = {};
+                          toSet['widgets.' + widgetId + '.height'] = ui.size.height;
+                          toSet['widgets.' + widgetId + '.width'] = ui.size.width;
+                          Dashboards.update(Session.get("db")._id, { $set: toSet });
+                      }
+            });
+
+        }
     },
 
     'mouseleave': function(e) {
@@ -54,6 +75,7 @@ Template.widget.events({
 
 Template.widget.rendered = function() {
     var idName = "#widget_" + this.data.widgetId;
+    var widget = this;
 
     $(idName).resizable({
         stop: function(event, ui) {
@@ -65,13 +87,14 @@ Template.widget.rendered = function() {
         }
     });
 
+    $(".stickyEdit").unbind("keypress");
+    $(".stickyEdit").unbind("blur");
+
     // Able to edit..
     $(".stickyEdit").keypress(function(e) {
         if (e.charCode == 13) {
-            widgetId = $(this).parent().attr('id').substring(7);
-            toSet = {};
-            toSet['widgets.' + widgetId + '.data.content'] = $(this).val();
-            Dashboards.update(Session.get("db")._id, { $set: toSet });
+            widget.data.save($(this).val());
+
         }
     });
 }
