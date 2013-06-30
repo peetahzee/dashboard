c8a7a4432f54558e58efd55483a620c01a07da88
Ali Nahm <alisnahm@gmail.com>
2013-06-30 01:57:46 -0700

    resizing now works and cleaned up a bit of code

--- widgetTypes/scoreBoard.js
+++ widgetTypes/scoreBoard.js
@@ -17,86 +17,90 @@ _.extend(ScoreBoard.prototype, {
         this.render();
         console.log("need to do getdta function");
     },
+    clicked: function() {
+        console.log('blargl');
+    },
     rendered: function() {
-                  var self = this;
-                  self.setupResizeDragDelete();
+        var self = this;
+        self.setupResizeDragDelete();
 
-
-
-                  if (!self.drawBarGraph) {
-                      self.drawBarGraph = Meteor.autorun(function() {
-                          //Width and height
-                          widgetDivId = "#widget_" + self.widgetId;
-
-                          // TODO get rid of automatic hard coding here..
-                          var w = 500;
-                          var bottomHeight = 30;
-                          var h = 300; //$(widgetDivId).height();
-                          var keys = [];
-                          var values = [];
-
-                          $.each(self.data.content, function(item) {
-                              keys.push(item);
-                              values.push(self.data.content[item]);
-                          });
-
-                          var valuesMax = Math.max.apply(Math, values);
-
-                          //Create SVG element
-                          var svg = d3.select(widgetDivId)
-                          .append("svg")
-                          .attr("width", w)
-                          .attr("height", h + bottomHeight);
-
-                          svg.selectAll("rect")
-                              .data(values)
-                              .enter()
-                              .append("rect")
-                              .attr("x", function(d, i) {
-                                  return i * (w / values.length);
-                              })
-                              .attr("y", function(d) {
-                                  return h - ((d * h) / valuesMax);
-                              })
-                              .attr("width", w / values.length - 5)
-                              .attr("height", function(d) {
-                                  return ((d * h) / valuesMax);
-                              });
-
-                          svg.selectAll("text")
-                              .data(values)
-                              .enter()
-                              .append("text")
-                              .text(function(d) {
-                                  return d;
-                              })
-                              .attr("x", function(d, i) {
-                                  return i * (w / values.length) + (w / values.length - 5) / 2;
-                              })
-                              .attr("y", function(d) {
-                                  return h - ((d * h) / valuesMax) + 14;
-                              })
-                              .attr("font-family", "sans-serif")
-                              .attr("font-size", "14px")
-                              .attr("fill", "black")
-                              .attr("text-anchor", "middle")
-
-                         svg.selectAll("text.name")
-                              .data(keys)
-                              .enter().append("text")
-                              .attr("x", function(d, i) {
-                                  return i * (w / keys.length) + (w / values.length - 5) / 2;
-                              })
-                              .attr("y", h + bottomHeight - 15)
-                              .attr("dy", ".36em")
-                              .attr("text-anchor", "middle")
-                              .attr('class', 'name')
-                              .text(String);
-
-                      });
-                  }
-              }
+        if (!self.drawBarGraph) {
+            self.drawBarGraph = Meteor.autorun(function() {
+                drawGraph(self)
+            });
+        }
+    }
 });
+
+var drawGraph = function(self) {
+    //Width and height
+    widgetDivId = "#widget_" + self.widgetId;
+
+    var w = $(widgetDivId).width();
+    var bottomHeight = 60;
+    var h = $(widgetDivId).height() - 2 * bottomHeight;
+    var keys = [];
+    var values = [];
+
+    $.each(self.data.content, function(item) {
+        keys.push(item);
+        values.push(self.data.content[item]);
+    });
+
+    var valuesMax = Math.max.apply(Math, values);
+
+    //Create SVG element
+    var svg = d3.select(widgetDivId)
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h + bottomHeight);
+
+    svg.selectAll("rect")
+        .data(values)
+        .enter()
+        .append("rect")
+        .attr("x", function(d, i) {
+            return i * (w / values.length);
+        })
+    .attr("y", function(d) {
+        return h - ((d * h) / valuesMax);
+    })
+    .attr("width", w / values.length - 5)
+        .attr("height", function(d) {
+            return ((d * h) / valuesMax);
+        });
+
+    svg.selectAll("text")
+        .data(values)
+        .enter()
+        .append("text")
+        .text(function(d) {
+            return d;
+        })
+    .attr("x", function(d, i) {
+        return i * (w / values.length) + (w / values.length - 5) / 2;
+    })
+    .attr("y", function(d) {
+        return h - ((d * h) / valuesMax) + 14;
+    })
+    .attr("font-family", "sans-serif")
+        .attr("font-size", "14px")
+        .attr("fill", "black")
+        .attr("text-anchor", "middle")
+
+        svg.selectAll("text.name")
+        .data(keys)
+        .enter().append("text")
+        .attr("x", function(d, i) {
+            return i * (w / keys.length) + (w / values.length - 5) / 2;
+        })
+    .attr("y", h + bottomHeight - bottomHeight / 2)
+        .attr("dy", ".36em")
+        .attr("text-anchor", "middle")
+        .attr('class', 'name')
+        .text(String);
+}
+
 
 function updateScoreBoard(id) {
     console.log("need update?");
